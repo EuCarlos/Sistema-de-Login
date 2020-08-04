2156c88a0ab819a3a19e76ac2b8404e92b4ec900
Carlos Alves <carlosalves369m@hotmail.com>
2020-08-04 13:01:30 -0300

    gerando um banco SQLite3

--- banco.py
+++ banco.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-conn = sqlite3.connect("recursos/UserData.db")
+conn = sqlite3.connect("UserData.db")
 cursor = conn.cursor()
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS Users(
